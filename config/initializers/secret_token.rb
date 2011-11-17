85e14b3ee0de292c3cc1ef320d70ca80a7ba385a
kientri <khoi.ntm@gmail.com>
2011-11-18 10:53:58 +1100

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticktee::Application.config.secret_token = '810eee4d92be9a2a76577275deb082f34afb1063a707862d86c0e5b226ac74a256e826d9154023f3789e4a1938feab60bd0250707898aaf32803ded935dcb5a7'
